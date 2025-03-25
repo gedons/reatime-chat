8db4dd8f853bfd5eac76200cc75e4cce02f0090f
Nerdy Gedoni <gedoniani@gmail.com>
2025-03-25 09:34:29 +0100

    refactor: update header styling and replace SVG with image

--- src/views/Index.vue
+++ src/views/Index.vue
@@ -21,10 +21,8 @@
       }"
     >
       <div class="container mx-auto flex justify-between items-center">
-        <div class="text-2xl font-bold flex items-center space-x-2">
-          <svg class="w-8 h-8" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 12h.01M12 12h.01M16 12h.01M21 12c0 4.418-4.03 8-9 8a9.863 9.863 0 01-4.255-.949L3 20l1.395-3.72C3.512 15.042 3 13.574 3 12c0-4.418 4.03-8 9-8s9 3.582 9 8z"/>
-          </svg>
+        <div class="text-2xl font-semibold flex items-center space-x-2">          
+          <img class="w-8 h-8" src="/chat.svg"/>
           <span>ChatterBox</span>
         </div>
         
@@ -48,7 +46,7 @@
         </div>
 
         <!-- Mobile Menu Button -->
-        <button @click="isMenuOpen = !isMenuOpen" class="md:hidden p-2 rounded-lg hover:bg-gray-100 transition-colors">
+        <button @click="isMenuOpen = !isMenuOpen" class="md:hidden cursor-pointer p-2 rounded-lg transition-colors">
           <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"/>
           </svg>
